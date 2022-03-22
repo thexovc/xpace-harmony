6d9e92774b451c84812782f38195ae089dc47f79
thexovc <osazeepeter79@gmail.com>
2022-03-22 15:24:52 +0100

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['miro.medium.com', 'www.freecodecamp.org'],
+    // formats: ['image/avif', 'image/webp'],
+  },
 }
 
 module.exports = nextConfig
