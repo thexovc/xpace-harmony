b44841e91c6911536c800a2adcd17046dd04ec2d
thexovc <osazeepeter79@gmail.com>
2022-03-26 03:45:38 +0100

    third

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,9 @@ const Home = () => {
 
           </nav>
           <button onClick={createUser} className="inline-flex items-center text-white font-bold  bg-green-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Connect
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+            <svg fill="none" stroke="currentColor"
+
+              className="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
           </button>
@@ -90,7 +92,7 @@ const Home = () => {
             <p className="mb-8 leading-relaxed">
               Today the frontier is on the internet and even on the internet,
               the frontier is within Web3 and crypto because
-              it’s sort of the least regulated the most decentralized,
+              it &apos; s sort of the least regulated the most decentralized,
               most permissionless, 24x7x365 markets
               that are self-funding hackers from all around the world.</p>
 
@@ -98,7 +100,7 @@ const Home = () => {
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img className="object-cover object-center rounded" alt="hero"
               src="/assets/hero.jpg" />
-          </div>
+          </div> '
         </div>
       </section>
       <section className="text-gray-700 body-font border-t border-gray-200">
@@ -112,7 +114,9 @@ const Home = () => {
               <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                 <div className="flex items-center mb-3">
                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor"
+
+                      className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                     </svg>
                   </div>
@@ -128,7 +132,9 @@ const Home = () => {
               <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                 <div className="flex items-center mb-3">
                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor"
+
+                      className="w-5 h-5" viewBox="0 0 24 24">
                       <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                       <circle cx="12" cy="7" r="4"></circle>
                     </svg>
@@ -147,7 +153,9 @@ const Home = () => {
               <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
                 <div className="flex items-center mb-3">
                   <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor"
+
+                      className="w-5 h-5" viewBox="0 0 24 24">
                       <circle cx="6" cy="6" r="3"></circle>
                       <circle cx="6" cy="18" r="3"></circle>
                       <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
@@ -156,9 +164,9 @@ const Home = () => {
                   <h2 className="text-gray-900 text-lg title-font font-medium">Opportunity</h2>
                 </div>
                 <div className="flex-grow">
-                  <p className="leading-relaxed text-base">It's not even the first inning. It’s like the anthem before the game has even started.
+                  <p className="leading-relaxed text-base">It's not even the first inning. It &apos; s like the anthem before the game has even started.
 
-                    And I have never seen so many highly intelligent, ambitious, capable people drop whatever they’re doing, in many cases really attractive things, to dedicate their time to this.</p>
+                    And I have never seen so many highly intelligent, ambitious, capable people drop whatever they &apos; re doing, in many cases really attractive things, to dedicate their time to this.</p>
 
                 </div>
               </div>
@@ -187,18 +195,20 @@ const Home = () => {
 
       {/* modal start */}
 
-      {toggle && <div id="popup-modal" tabindex="-1" className="flex justify-center w-full h-full items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+      {toggle && <div id="popup-modal" tabIndex="-1" className="flex justify-center w-full h-full items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
         <div className="relative p-4 w-full max-w-md h-full md:h-auto ">
 
           <div className="relative bg-white border-2 border-indigo-900 rounded-lg shadow dark:bg-gray-700">
 
             <div className="flex justify-end p-2">
               <button onClick={() => setToggle(false)} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="popup-modal">
-                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
               </button>
             </div>
             <div className="p-6 pt-0 text-center">
-              <svg className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <svg className="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
+
+                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
               <h3 className="mb-5 text-lg text-gray-500 font-bold dark:text-gray-700">Solana wallet not detected</h3>
               <h3 className="mb-5 text-xl font-normal text-gray-500 dark:text-gray-400">You must have a solana wallet to continue</h3>
               <button onClick={() => setToggle(false)} data-modal-toggle="popup-modal" type="button" className="font-bold text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800  rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
